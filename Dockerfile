f50b8b718acee1357429f344204838ef5de86864
luisfrodriguezicasa <lrodriguez@martinexsa.com>
2025-12-11 18:55:16 -0500

    Ajuste inicial 1.0.2

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,12 @@
-# Imagen base de Python
 FROM python:3.10-slim
 
-# Crear directorio de app
 WORKDIR /app
 
-# Copiar dependencias
 COPY requirements.txt .
-
-# Instalar dependencias
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copiar todo el proyecto
 COPY . .
 
-# Exponer puerto estándar de Cloud Run
 EXPOSE 8080
 
-# Comando de ejecución
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-import requests
-from flask import Flask, request, Response
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-# TEMPORAL: Para validar la IP pública del NAT
-TARGET_URL = "https://ifconfig.me"
+@app.get("/")
+def home():
+    return {"status": "ok", "msg": "Cloud Run funcionando"}
 
-@app.route("/", methods=["GET", "POST"])
-def proxy():
-    resp = requests.request(
-        method=request.method,
-        url=TARGET_URL,
-        headers={k: v for k, v in request.headers if k.lower() != "host"},
-        params=request.args,
-        data=request.get_data()
-    )
-
-    excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
-    headers = [(name, value) for name, value in resp.raw.headers.items() if name.lower() not in excluded_headers]
-
-    return Response(resp.content, resp.status_code, headers)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
